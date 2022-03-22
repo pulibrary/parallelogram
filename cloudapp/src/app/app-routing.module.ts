7e090f9e644c93a9eb97f83edd430f0b0f61fc15
Thomas P. Ventimiglia <tventimi@princeton.edu>
2022-03-22 08:07:07 -0400

    remove details component

--- cloudapp/src/app/app-routing.module.ts
+++ cloudapp/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { SettingsComponent } from './settings/settings.component';
 import { TopmenuComponent } from './topmenu/topmenu.component';
-import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'settings', component: SettingsComponent},
   { path: 'topmenu', component: TopmenuComponent},
-  { path: 'detail/:mms_id/:field_id', component: DetailComponent},
 ];
 
 @NgModule({
