476bb30a8251158779792dc901249cbb9b387ba7
Thomas P. Ventimiglia <tventimi@princeton.edu>
2022-06-08 13:49:04 -0400

    upgrade to latest version of eca

--- cloudapp/src/app/app.module.ts
+++ cloudapp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import { MaterialModule, getTranslateModule, AlertModule } from '@exlibris/exl-cloudapp-angular-lib';
+import { MaterialModule, CloudAppTranslateModule, AlertModule } from '@exlibris/exl-cloudapp-angular-lib';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -26,7 +26,7 @@ import { SettingsComponent } from './settings/settings.component';
     AlertModule,
     FormsModule,
     ReactiveFormsModule,
-    getTranslateModule(),
+    CloudAppTranslateModule.forRoot(),
   ],
   providers: [
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'standard' } },
