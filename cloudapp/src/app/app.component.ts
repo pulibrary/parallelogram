93690649f5d629e793913c94b044db0e659076e4
Tom Ventimiglia <tventimi@princeton.edu>
2026-04-15 08:08:49 -0400

    move alert position to top

--- cloudapp/src/app/app.component.ts
+++ cloudapp/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
-  template: '<cloudapp-alert></cloudapp-alert><router-outlet></router-outlet>'
+  template: '<cloudapp-alert></cloudapp-alert><router-outlet></router-outlet>',
+  styles: `cloudapp-alert {
+    top: 0;
+    bottom: auto;
+    position: relative;
+  }`
 })
 export class AppComponent {
 
